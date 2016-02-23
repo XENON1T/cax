4472a15e5fe14818c730149c6beb99640773017a
Christopher Tunnell <ctunnell@nikhef.nl>
2016-02-23 12:17:00 +0100

    Process and cleanup

--- cax/process.py
+++ cax/process.py
@@ -1,18 +1,52 @@
-from paramiko import SSHClient, util
-from scp import SCPClient
 import config
-import os
-import tempfile
-import argparse
-import subprocess
 import qsub
+import sys
+from pax import core
 from pax import __version__ as pax_version
 
-def process():
-    pax_instance = core.Processor(config_names=config)
-    pass
+def process(name, location, config='XENON1T_LED'):
+    # Grab the Run DB so we can query it
+    collection = config.mongo_collection()
 
-def submit(name, location, config='XENON1T_LED'):
+    # New data
+    datum = {'type' : 'processed',
+             'host' : config.get_hostname(),
+             'status' : 'processing',
+             'location' : location + '.root',
+             'checksum' : None,
+             'pax_version' : pax_version}
+    query = {'detector' : 'tpc', 'name' : name}
+    collection.update(query,
+                      {'$push': {'data': datum}})
+
+    query['data.type'] = 'processed'
+    query['data.host'] = 'host'
+    query['data.pax_version'] = pax_version
+
+    try:
+        print('processing', name, location)
+        p = core.Processor(config_names=config,
+                                      config_dict={'pax': { 'input_name':  location,
+                                                            'output_name': location}})
+        p.run()
+    except Exception as exception:
+        datum['status'] = 'error'
+        collection.update(query,
+                          {'$set': {'data.$' : datum}})
+        raise
+
+    datum['status'] = 'verifying'
+    collection.update(query,
+                       {'$set': {'data.$' : datum}})
+
+    if verify():
+        datum['status'] = 'processed'
+    else:
+        datum['status'] = 'failed'
+    collection.update(query,
+                       {'$set': {'data.$' : datum}})
+
+def submit(name, location):
     '''Submit XENON100 pax processing jobs to ULite
     Author: Chris, Bart, Jelle, Nikhef
     Last update:   2015.09.07
@@ -25,12 +59,10 @@ def submit(name, location, config='XENON1T_LED'):
     script_template = """#!/bin/bash
 export PATH=/data/xenon/anaconda/envs/pax/bin:$PATH
 source activate pax
-echo paxer --input {location} --output {location} --config {config}
-paxer --input {location} --output {location} --config {config}
+python /user/ctunnell/cax/cax/process.py {name} {location}
     """
 
-    script = script_template.format(location=location,
-                                    config=config)
+    script = script_template.format(name=name, location=location)
     qsub.submit_job(script, name, 'generic')
 
 
@@ -66,39 +98,14 @@ def process_all():
                 have_processed = True
 
         if have_raw and not have_processed:
-            local_config = config.get_config(config.get_hostname())
-
-            # New data
-            datum = {'type' : 'processed',
-                     'host' : config.get_hostname(),
-                     'status' : 'processing',
-                     'location' : have_raw['location'] + '.root',
-                     'checksum' : None,
-                     'pax_version' : pax_version}
-            #collection.update({'_id': doc['_id']},
-            #                  {'$push': {'data': datum}})
-            print('processing', doc['name'], have_raw['location'])
             submit(doc['name'],
                    have_raw['location'])
-
-            datum['status'] = 'verifying'
-            #collection.update({'_id': doc['_id'],
-            #                   'data.host' : datum['host'],
-            #                   'data.type' : datum['type']},
-            #                   {'$set': {'data.$' : datum}})
-
-            if verify():
-                datum['status'] = 'processed'
-            else:
-                datum['status'] = 'failed'
-            #collection.update({'_id': doc['_id'],
-            #                   'data.host' : datum['host'],
-            #                   'data.type' : datum['type']},
-            #                   {'$set': {'data.$' : datum}})
-
 
 
 
 
 if __name__ == "__main__":
-    process_all()
+    if len(sys.argv) == 1:
+        process_all()
+    else:
+        process(sys.argv[1], sys.argv[2])
