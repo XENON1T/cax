4101be877b36a0aa261b0eafb96d440789ec805c
Christopher Tunnell <ctunnell@nikhef.nl>
2016-04-08 13:25:13 +0200

    Only issue stale transfer alerts for non-transferred files

--- cax/tasks/clear.py
+++ cax/tasks/clear.py
@@ -25,17 +25,21 @@ class ClearDAQBuffer(checksum.CompareChecksums):
             self.remove_untriggered()
 
 class ClearFailedTransfer(Task):
-    "Perform a checksum on accessible data."
+    "Alert if stale transfer."
 
     def each_location(self, data_doc):
         if 'host' not in data_doc or data_doc['host'] != config.get_hostname():
-            return
+            return # Skip places where we can't locally access data
 
+        # How long has transfer been ongoing
         time = data_doc['creation_time']
         difference = datetime.datetime.utcnow() - time
 
+        if data_doc["status"] != "transferred":
+            return # Transfer went fine
+
         self.log.debug(difference)
 
-        if difference > datetime.timedelta(days=1):
+        if difference > datetime.timedelta(days=1):  # If stale transfer
             self.give_error("Transfer lasting more than one day")
 
