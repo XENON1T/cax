85b3d492a64e5824a479ef59e925a3493df3aa99
Patrick De Perio <pdeperio@midway-login1.rcc.local>
2016-05-09 12:27:36 -0500

    Revert "Update process.py"
    This reverts commit b7dbeeaa48f43432502727619e1e32ef55e9d6f6.
    
    The previous causes an error "Unknown config host: midway-login1" and also all the non-process tasks were commented out. Should make pull request for review prior to committing to head.

--- cax/tasks/process.py
+++ cax/tasks/process.py
@@ -1,34 +1,11 @@
 import sys
 import datetime
 import hashlib
-import subprocess
 
 from pax import __version__ as pax_version
 
 from cax import qsub, config
 from cax.task import Task
-
-# Obtain pax repository hash
-def get_pax_hash(pax_version, processat):
-
-    PAX_DEPLOY_DIR = ''
-    if processat == 'midway-login-1':
-        # Location of GitHub source code on Midway
-        PAX_DEPLOY_DIR="/project/lgrandi/deployHQ/pax"
-    if processat == 'tegner-login-1':
-        # Location of GitHub source code on Midway
-        PAX_DEPLOY_DIR="/afs/pdc.kth.se/projects/xenon/software/pax"
-
-    # Get hash of this pax version
-    if pax_version == 'head':
-        git_args = "--git-dir="+PAX_DEPLOY_DIR+"/.git rev-parse HEAD"
-    else:
-        git_args = "--git-dir="+PAX_DEPLOY_DIR+"/.git rev-parse "+pax_version
-
-    git_out = subprocess.check_output("git "+git_args, shell=True)
-    pax_hash = git_out.rstrip().decode('ascii')
-
-    return pax_hash
 
 def filehash(location):
     sha = hashlib.sha512()
@@ -42,7 +19,7 @@ def filehash(location):
 def verify():
     return True
 
-def process(name, location, host, pax_version, pax_hash, out_location):
+def process(name, location, host):
     from pax import core
     # Grab the Run DB so we can query it
     collection = config.mongo_collection()
@@ -54,15 +31,13 @@ def process(name, location, host, pax_version, pax_hash, out_location):
              'location'   : location + '.root',
              'checksum'   : None,
              'pax_version': pax_version,
-             'pax_has': pax_hash,
              'creation_time' : datetime.datetime.utcnow()}
     query = {'detector': 'tpc',
              'name'    : name}
 
-
     if collection.find_one({'detector': 'tpc',
                             'name' : name,
-                            "data": { "$elemMatch": { "host": datum['host'],
+                            "data": { "$elemMatch": { "host": "midway-login1",
                                                       "type": "processed",
                                                       "status": "transferred"}}}) is not None:
         print("Abort", name, "already processed")
@@ -70,7 +45,7 @@ def process(name, location, host, pax_version, pax_hash, out_location):
 
     elif collection.find_one({'detector': 'tpc',
                             'name' : name,
-                            "data": { "$elemMatch": { "host": datum['host'],
+                            "data": { "$elemMatch": { "host": "midway-login1",
                                                       "type": "processed",
                                                       "status": "transferring"}}}) is not None:
         print("Abort", name, "already currently processing")
@@ -119,9 +94,10 @@ def process(name, location, host, pax_version, pax_hash, out_location):
 class ProcessBatchQueue(Task):
     "Perform a checksum on accessible data."
 
-    def submitMidway(self, in_location, host, pax_version, pax_hash, out_location):
-        '''Midway Submission Script
-        Last update:   2016.04.29
+    def submit(self, location, host):
+        '''Submit XENON100 pax processing jobs to ULite
+        Author: Chris, Bart, Jelle, Nikhef
+        Last update:   2015.09.07
         '''
         name = self.run_doc['name']
         run_mode = ''
@@ -131,63 +107,28 @@ class ProcessBatchQueue(Task):
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --account=pi-lgrandi
-
-echo "nprocs = `nproc`"
 export PATH=/project/lgrandi/anaconda3/bin:$PATH
-source activate pax_{pax_version}
-cd /project/lgrandi/xenon1t/processing
-echo /home/pdeperio/160429-cax_processing/cax/cax/tasks/process.py {name} {in_location} {host} {pax_version} {pax_hash} {out_location}
-python /home/pdeperio/160429-cax_processing/cax/cax/tasks/process.py {name} {in_location} {host} {pax_version} {pax_hash} {out_location}
-        """
-
-        script = script_template.format(name=name, in_location=in_location, host=host, pax_version=pax_version, pax_hash=pax_hash, out_location=out_location)
-        print ("fake qsub ", script)
-        #qsub.submit_job(script, name, 'midway-login-1')
-
-
-    def submitPDC(self, in_location, host, pax_version, pax_hash, out_location):
-        '''Stockholm Submission Script
-        '''
-        name = self.run_doc['name']
-        run_mode = ''
-        script_template = """#!/bin/bash
-#SBATCH --job-name=Xe1tReprocessing
-#SBATCH --partition=main
-#SBATCH --output=/cfs/klemming/projects/xenon/common/xenon1t/slurm_reprocessing_logs/{name}_output.log
-#SBATCH --error=/cfs/klemming/projects/xenon/common/xenon1t/slurm_reprocessing_logs/{name}_error.log
-#SBATCH -A xenon
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=32
-#SBATCH -t 72:00:00
-#SBATCH --mem=24000
-#SBATCH --mail-user=Boris.Bauermeister@fysik.su.se
-#SBATCH --mail-type=ALL
-source /afs/pdc.kth.se/home/b/bobau/load_4.8.4.sh
 source activate pax_head
-cd /cfs/klemming/projects/xenon/xenon1t/root/pax_head
-echo python /afs/pdc.kth.se/projects/xenon/software/cax/cax/tasks/process.py {name} {in_location} {host} {pax_version} {pax_hash} {out_location}
-python /afs/pdc.kth.se/projects/xenon/software/cax/cax/tasks/process.py {name} {in_location} {host} {pax_version} {pax_hash} {out_location}
+export PROCESSING_DIR=/project/lgrandi/xenon1t/processing
+export OUTPUT_DIR=${{PROCESSING_DIR}}/{name}
+mkdir -p ${{OUTPUT_DIR}}
+cd ${{OUTPUT_DIR}}
+echo time python /project/lgrandi/deployHQ/cax/cax/tasks/process.py {name} {location} {host}
+time python /project/lgrandi/deployHQ/cax/cax/tasks/process.py {name} {location} {host}
+mv ${{PROCESSING_DIR}}/logs/{name}_*.log ${{OUTPUT_DIR}}/.
         """
-        script = script_template.format(name=name, in_location=in_location, host=host, pax_version=pax_version, pax_hash=pax_hash, out_location=out_location)
-        print ("fake qsub ", script)
-        #qsub.submit_job(script, name, 'tegner-login-1')
+
+        script = script_template.format(name=name, location=location, host=host)
+        self.log.info(script)
+        qsub.submit_job(script, name)
 
     def verify(self):
         """Verify processing worked"""
         return True  # yeah... TODO.
 
     def each_run(self):
-        
-        # Get desired pax versions and corresponding output directories
-        versions = config.get_pax_options('versions')
-        out_locations = config.get_pax_options('locations')
-        #print( versions, out_locations)
-        
-        have_processed = [False for i in range(len(versions))]
         have_raw = False
-                
-        #have_raw = False
-        #have_processed = False
+        have_processed = False
 
         # Iterate over data locations to know status
         for datum in self.run_doc['data']:
@@ -197,7 +138,6 @@ python /afs/pdc.kth.se/projects/xenon/software/cax/cax/tasks/process.py {name} {
 
             # If the location doesn't refer to here, skip
             if datum['host'] != config.get_hostname():
-                print( datum['host'])
                 continue
 
             if datum['type'] == 'raw':
@@ -208,46 +148,22 @@ python /afs/pdc.kth.se/projects/xenon/software/cax/cax/tasks/process.py {name} {
                 if datum['status'] != 'error':
                     have_processed = True
 
-        # Process all specified versions
-        for ivers in range(len(versions)):
-            #print(config.get_hostname())
-            
-            pax_version=versions[ivers]
-            pax_hash = get_pax_hash(pax_version, config.get_hostname())
-            out_location=out_locations[ivers]
-
-            if have_raw and not have_processed:
-                if self.run_doc['reader']['ini']['write_mode'] == 2:
-                
-                  if config.get_hostname() == "tegner-login-1":
-                    print('tegner')
-                    # Get number of events in data set
-                    events = self.run_doc.get('trigger', {}).get('events_built', 0) 
-                
-                    # Only submit jobs for datasets with events
-                    if events > 0:
-                        self.log.info("Submitting %s (@Tegner)", self.run_doc['name'], pax_version, pax_hash, out_location)
-                    
-                        self.submitPDC(have_raw['location'], have_raw['host'], pax_version, pax_hash, out_location)
-
-                    else:
-                        self.log.info("Skipping %s with 0 events", self.run_doc['name'])
-                
-                
-                  if config.get_hostname() == "midway-login1":
+        if have_raw and not have_processed:
+            if self.run_doc['reader']['ini']['write_mode'] == 2:
+                if config.get_hostname() == "midway-login1":
 
                     # Get number of events in data set
                     events = self.run_doc.get('trigger', {}).get('events_built', 0) 
                 
                     # Only submit jobs for datasets with events
                     if events > 0:
-                        self.log.info("Submitting %s (@Tegner)", self.run_doc['name'], pax_version, pax_hash, out_location)
+                        self.log.info("Submitting %s", self.run_doc['name'])
                     
-                        self.submitMidway(have_raw['location'], have_raw['host'], pax_version, pax_hash, out_location)
+                        self.submit(have_raw['location'], have_raw['host'])
 
                     else:
                         self.log.info("Skipping %s with 0 events", self.run_doc['name'])
 
 
 if __name__ == "__main__":
-    process(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6])
+    process(sys.argv[1], sys.argv[2], sys.argv[3])
