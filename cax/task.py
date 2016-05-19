76d8dab07841bce8936ad7075df12e6ef1763ca5
Christopher Tunnell <ctunnell@nikhef.nl>
2016-05-19 15:22:42 +0200

    Only copy runs above 500

--- cax/task.py
+++ cax/task.py
@@ -18,7 +18,7 @@ class Task():
         # Collect all run documents.  This has to be turned into a list
         # to avoid timeouts if a task takes too long.
         docs = list(self.collection.find({'detector': 'tpc',
-                                          'number' : 51}))
+                                          'number' : {"$gt": 500}}))
 
         for doc in docs:
             # Make sure up to date
