e3c881b62a42dda07f210c2cbd4a2ff5416b7717
Patrick de Perio <pdeperio@users.noreply.github.com>
2016-05-20 13:11:00 -0400

    Update process.py
    cleaner command line argument handling

--- cax/tasks/process.py
+++ cax/tasks/process.py
@@ -290,5 +290,5 @@ if __name__ == "__main__":
     main() 
 
 def main():
-    process(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7])
+    process(*sys.argv[1:])
 
