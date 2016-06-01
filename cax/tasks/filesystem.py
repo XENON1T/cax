c7e1f3aa719731443807a39dfddab13fa6ff6f7e
Christopher Tunnell <ctunnell@nikhef.nl>
2016-06-01 14:36:17 +0200

    Cleanup filesystem task and comment.

--- cax/tasks/filesystem.py
+++ cax/tasks/filesystem.py
@@ -1,60 +1,22 @@
-import logging
+"""File system operations through cax
+
+This allows you to move or remove files while still notifying the run database.
+"""
+
 import os
 import shutil
-
-import scp
-from paramiko import SSHClient, util
 
 from cax import config
 from cax.task import Task
 
 
-def copy(datum_original, datum_destination):
-    util.log_to_file('ssh.log')
-    ssh = SSHClient()
-    ssh.load_system_host_keys()
-
-    if datum_original['host'] == config.get_hostname():
-        upload = True
-
-        config_destination = config.get_config(datum_destination['host'])
-        server = config_destination['hostname']
-        username = config_destination['username']
-
-    elif datum_destination['host'] == config.get_hostname():
-        upload = False  # ie., download
-
-        config_original = config.get_config(datum_original['host'])
-        server = config_original['hostname']
-        username = config_original['username']
-    else:
-        raise ValueError()
-
-    logging.info("connection to %s" % server)
-    ssh.connect(server,
-                username=username,
-                compress=True)
-
-    # SCPCLient takes a paramiko transport as its only argument
-    client = scp.SCPClient(ssh.get_transport())
-
-    if upload:
-        logging.info("put: %s to %s" % (datum_original['location'],
-                                        datum_destination['location']))
-        client.put(datum_original['location'],
-                   datum_destination['location'],
-                   recursive=True)
-    else:
-        logging.info("get: %s to %s" % (datum_original['location'],
-                                        datum_destination['location']))
-        client.get(datum_original['location'],
-                   datum_destination['location'],
-                   recursive=True)
-
-    client.close()
-
-
 class RenameSingle(Task):
+    """Rename a file
+
+    This renames a file or folder then updates the run database to reflect it.
+    This is an unsafe operation since it does not perform a new checksum.
+    """
+
     def __init__(self, input, output):
         # Save filesnames to use
         self.input = os.path.abspath(input)
@@ -64,10 +26,11 @@ class RenameSingle(Task):
         Task.__init__(self)
 
     def each_run(self):
-       # For each data location, see if this filename in it
-       for data_doc in self.run_doc['data']:
+        # For each data location, see if this filename in it
+        for data_doc in self.run_doc['data']:
             # Is not local, skip
-            if 'host' not in data_doc or data_doc['host'] != config.get_hostname():
+            if 'host' not in data_doc or \
+                            data_doc['host'] != config.get_hostname():
                 continue
 
             if data_doc['location'] != self.input:
@@ -75,17 +38,25 @@ class RenameSingle(Task):
 
             self.log.info("Moving %s to %s" % (self.input,
                                                self.output))
-            os.rename(self.input,
+            # Perform renaming
+            os.renames(self.input,
                       self.output)
 
+            # Notify run database
             if config.DATABASE_LOG is True:
                 self.collection.update({'_id' : self.run_doc['_id'],
                                         'data': {'$elemMatch': data_doc}},
-                                       {'$set': {'data.$.location': self.output}})
+                                       {'$set': {
+                                           'data.$.location': self.output}})
             break
 
 
 class RemoveSingle(Task):
+    """Remove a single file or directory
+
+    This notifies the run database.
+    """
+
     def __init__(self, location):
         # Save filesnames to use
         self.location = os.path.abspath(location)
@@ -94,19 +65,22 @@ class RemoveSingle(Task):
         Task.__init__(self)
 
     def each_run(self):
-       # For each data location, see if this filename in it
-       for data_doc in self.run_doc['data']:
+        # For each data location, see if this filename in it
+        for data_doc in self.run_doc['data']:
             # Is not local, skip
-            if 'host' not in data_doc or data_doc['host'] != config.get_hostname():
+            if 'host' not in data_doc or \
+                            data_doc['host'] != config.get_hostname():
                 continue
 
             if data_doc['location'] != self.location:
                 continue
 
+            # Notify run database
             if config.DATABASE_LOG is True:
                 self.collection.update({'_id': self.run_doc['_id']},
-                                        {'$pull': {'data': data_doc}})
+                                       {'$pull': {'data': data_doc}})
 
+            # Perform operation
             self.log.info("Removing %s" % (self.location))
             if os.path.isdir(data_doc['location']):
                 shutil.rmtree(data_doc['location'])
