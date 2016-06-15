b5d39314853307a4005111bda79c83e3d2ce03ce
Christopher Tunnell <ctunnell@nikhef.nl>
2016-06-15 16:09:52 +0200

    Purity function change

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('HISTORY.rst') as history_file:
 
 requirements = [
     'checksumdir', 'scp', 'pagerduty-api', 'pymongo', 'paramiko',
-    'numpy'
+    'numpy', 'sympy'
 ]
 
 test_requirements = [
