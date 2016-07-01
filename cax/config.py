0ef121625ff7fd439a0ffb1ae232cb1c50e2c63c
Christopher Tunnell <ctunnell@nikhef.nl>
2016-07-01 10:51:24 +0200

    Update email

--- cax/config.py
+++ cax/config.py
@@ -195,6 +195,7 @@ def processing_script(args={}):
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task={ncpus}
 #SBATCH --mail-type=ALL
+#SBATCH --mail-user=xe-computing-status@lngs.infn.it
 
 #SBATCH --output={base}/{use}/logs/{number}_{pax_version}_%J.log
 #SBATCH --error={base}/{use}/logs/{number}_{pax_version}_%J.log
