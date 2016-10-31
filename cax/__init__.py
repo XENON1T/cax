e6f576faa7c62c8d81570a8a549cc37704745bb1
Christopher Tunnell <ctunnell@nikhef.nl>
2016-10-31 07:56:29 -0500

    Bump version: 4.4.16 → 4.5.0

--- cax/__init__.py
+++ cax/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = 'Christopher Tunnell'
 __email__ = 'ctunnell@nikhef.nl'
-__version__ = '4.4.16'
+__version__ = '4.5.0'
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 PROJECT = 'cax'
-VERSION = '4.4.16'
+VERSION = '4.5.0'
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
