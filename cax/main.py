92e74530d64e4130e978fc8ec4d9b1d418c7c607
Francesco Lombardi <francesco.lombardi1@gmail.com>
2017-03-15 15:44:18 -0700

    Enabled BufferPurge again (#88)

--- cax/main.py
+++ cax/main.py
@@ -114,7 +114,8 @@ def main():
         filesystem.SetPermission(),  # Set any permissions (primarily for Tegner) for new data to make sure analysts can access
 
         clear.PurgeProcessed(), #Clear the processed data for a given version
-        # clear.BufferPurger(),  # Clear old data at some locations as specified in cax.json
+        clear.BufferPurger(),  # Clear old data at some locations as specified in cax.json
+
         process.ProcessBatchQueue(),  # Process the data with pax
         process_hax.ProcessBatchQueueHax()  # Process the data with hax
     ]
