7c9d78b5d358c899fb215e565c51e616b67aa64d
Patrick de Perio <pdeperio@users.noreply.github.com>
2017-05-31 17:58:42 -0400

    Restore purge tasks

--- cax/main.py
+++ cax/main.py
@@ -117,9 +117,10 @@ def main():
         checksum.AddChecksum(),  # Add checksum for data here so can know if corruption (useful for knowing when many good copies!)
 
         filesystem.SetPermission(),  # Set any permissions (primarily for Tegner) for new data to make sure analysts can access
-        # clear.BufferPurger(),  # Clear old data at some locations as specified in cax.json
         process.ProcessBatchQueue(),  # Process the data with pax
-        process_hax.ProcessBatchQueueHax()  # Process the data with hax
+        process_hax.ProcessBatchQueueHax(),  # Process the data with hax
+	clear.PurgeProcessed(), #Clear the processed data for a given version		
+        clear.BufferPurger()  # Clear old data at some locations as specified in cax.json    
     ]
 
     # Raises exception if unknown host
