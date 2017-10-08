1f5682a5e01b65afc5b26af5642c2b86b3b19983
Fei Gao <feigao.ge@gmail.com>
2017-10-08 12:14:50 -0500

    add ac analysis treemaker in the list

--- cax/tasks/process_hax.py
+++ cax/tasks/process_hax.py
@@ -41,7 +41,7 @@ def _process_hax(name, in_location, host, pax_version,
         init_hax(in_location, pax_version, out_location)   # may initialize once only
         hax.minitrees.load_single_dataset(name, ['Corrections', 'Basics', 'Fundamentals',
                                                  'DoubleScatter', 'LargestPeakProperties',
-                                                 'TotalProperties',  'Extended', 'Proximity'])
+                                                 'TotalProperties',  'Extended', 'Proximity','LoneSignalsPreS1', 'LoneSignals'])
 
     except Exception as exception:
         raise
