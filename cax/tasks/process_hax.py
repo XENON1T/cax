9a50e3c189832173db02e0a00ea8550d0552db0f
Patrick de Perio <pdeperio@users.noreply.github.com>
2018-02-02 11:02:31 -0500

    Add SE (IsolatedPeaks) treemaker

--- cax/tasks/process_hax.py
+++ cax/tasks/process_hax.py
@@ -41,7 +41,7 @@ def _process_hax(name, in_location, host, pax_version,
     print('creating hax minitrees for run', name, pax_version, in_location, out_location)
 
     TREEMAKERS = ['Corrections', 'Basics', 'Fundamentals', 'PositionReconstruction',
-                  'CorrectedDoubleS1Scatter', 'LargestPeakProperties',
+                  'CorrectedDoubleS1Scatter', 'LargestPeakProperties', 'IsolatedPeaks',
                   'TotalProperties', 'Extended', 'Proximity', 'LoneSignalsPreS1',
                   'LoneSignals', 'FlashIdentification']
 
